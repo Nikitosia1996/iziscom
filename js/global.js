8d4dfcd17905f2ea6fdcb15dead3f6556eea3ce4
Hancharou.Uladzislau <hancharou@rncpmt.by>
2024-04-26 14:18:50 +0300

    1 этап сохранение норм

--- js/global.js
+++ js/global.js
@@ -1,7 +1,17 @@
-var currentUrl = window.location.pathname;
+var currentUrl = window.location.search;
 
-if(currentUrl == "/" || currentUrl == "/index.php"){
+
+
+if(currentUrl == "?main" || currentUrl == ""){
     let mainMenu = document.querySelector('[href="index.php?main"]');
     mainMenu.classList.add('active');
+}
+else if(currentUrl == "/index.php?oborud"){
+    let oborMenu = document.querySelector('[href="index.php?oborud"]');
+    oborMenu.classList.add('active');
+}
+else if(currentUrl == "/index.php?servicemans"){
+    let serviceMenu = document.querySelector('[href="index.php?servicemans"]');
+    serviceMenu.classList.add('active');
 }
 
